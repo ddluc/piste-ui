54f91b572baebe6a26f5d9928cae9f331e35a905
Daniel Lucas <daniel@ddluc.com>
2022-08-16 11:25:30 -0700

    Implement Select Component

--- src/lib/components/Select/bin/Input.ts
+++ src/lib/components/Select/bin/Input.ts
@@ -1,28 +1,38 @@
-/* eslint-disable indent */
-import styled from 'styled-components';
+import styled, { Theme } from 'styled-components';
 import { px, getOutline } from '../../../util';
 
-export interface Props {
-  error?: boolean;
-  size?: 'sm' | 'normal' | 'lg';
+type SelectInputProps = {
+  error: boolean
+};
+
+export interface ThemedProps extends SelectInputProps {
+  theme: Theme;
 }
 
-const Input = styled.input<Props>`
+export const Input = styled.select<ThemedProps>`
   display: block;
-  margin-top: 4px;
+  width: 100%;
+  padding: 10px 8px 10px 8px;
   ${({ theme, error }) => {
     if (error) return `border: solid ${theme.border.width} ${theme.palette.danger}`;
     return `border: solid ${theme.border.width} ${theme.palette.neutral[2]}`;
   }};
   border-radius: ${({ theme }) => theme.border.radius};
-  color: ${({ theme, error }) => (error ? theme.palette.danger : theme.palette.neutral[0])};
-  padding: 10px 8px 10px 8px;
+  border-color: ${({ theme, error }) => (error ? theme.palette.danger : theme.palette.accent.main)};
+  outline: none;
+  background-color: ${({ theme }) => theme.palette.white};
+  margin-top: 4px;
   font-size: ${({ theme }) => px(theme.fonts.size.small)};
   font-weight: ${({ theme }) => theme.fonts.weight.normal};
-  outline: none;
-  width: 100%;
+  color: ${({ theme, error, value }) => {
+    if (error) return theme.palette.danger;
+    return (value === 'default' ? theme.palette.neutral[2] : theme.palette.neutral[0]);
+  }};
   box-sizing: border-box;
   transition: box-shadow .15s ease-in-out; 
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
 
   &:focus {
     border-color: ${({ theme, error }) => (error ? theme.palette.danger : theme.palette.accent.main)};
@@ -39,7 +49,5 @@ const Input = styled.input<Props>`
     font-size: ${({ theme }) => px(theme.fonts.size.small)};
     color: ${({ theme, error }) => (error ? theme.palette.danger : theme.palette.neutral[1])};
   }
-
+  
 `;
-
-export default Input;
