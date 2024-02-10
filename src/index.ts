10385c0c6f8c0a7c0d636238ba919bb2626d2303
Daniel Lucas <daniel@ddluc.com>
2024-02-10 08:58:02 -0800

    Export spacer component

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,7 @@ export { Scroll, ScrollProps } from './lib/components/Scroll';
 export { Select, SelectProps } from './lib/components/Select';
 export { Sidesheet, SidesheetProps } from './lib/components/Sidesheet';
 export { Skeleton, SkeletonProps } from './lib/components/Skeleton';
+export { Spacer, SpacerProps } from './lib/components/Spacer';
 export { default as Table, TableProps } from './lib/components/Table';
 export { TextInput, TextInputProps } from './lib/components/TextInput';
 export { Typography, TextProps } from './lib/components/Typography';
